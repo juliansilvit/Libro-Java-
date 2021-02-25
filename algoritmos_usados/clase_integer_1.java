aa0da5831bbff1d2badaac8be11a6e7456153606
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-02-24 21:57:24 -0500

    Create clase_integer_1.java

---
+++ algoritmos_usados/clase_integer_1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		//array creado con envoltorio Integer 
+		ArrayList<Integer> arr= new ArrayList<>();
+		//array creado con el dato primitivo int el cual genera error y fallo del código
+		ArrayList<int> arr2= new ArrayList<>();
+	}
+
+}
