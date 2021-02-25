166e9075cf40eff2272e40c1d0099ee143e47e64
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-02-24 22:04:45 -0500

    Create ciclo_dowhile.java

---
+++ algoritmos_usados/ciclo_dowhile.java
@@ -0,0 +1,14 @@
+public class app {
+	
+	public static void main(String[] args) {
+		int cont = 0;
+
+		do {
+			System.out.println("Aumentando... " + (cont + 1));
+
+			cont += 1;
+
+		} while (cont < 10);
+	}
+
+}
