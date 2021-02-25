e4dd8661bbf4e4e9be28f9dbfd3f14fa9a8664b7
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-02-24 22:04:22 -0500

    Create ciclo_while.java

---
+++ algoritmos_usados/ciclo_while.java
@@ -0,0 +1,18 @@
+CICLO WHILE
+
+
+public class app {
+	public static void main(String[] args) {
+		int i = 0;
+
+while (true) { //el true quiere decir que no se rompe hasta que se que le indique.
+			i++;
+			System.out.println("Valor de i: " + i);
+			if (i == 9) {
+				break;
+			}
+
+		}
+
+	}
+}
