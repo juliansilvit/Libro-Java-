b24f6490dbf0609bc3125b4ff870b0dba869f1b5
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-02-24 21:53:26 -0500

    Create entrada_console.java

---
+++ algoritmos_usados/entrada_console.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class codigos {
+	public static void main(String arg[]){
+		PrintWriter pw = new PrintWriter(System.out);
+		String str = System.console().readLine();
+        pw.println(str);
+	pw.close();
+	}
+}
