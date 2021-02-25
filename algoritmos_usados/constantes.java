4afeb37ff5148281b59a4eda2c0448f53e26fd24
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-02-24 22:00:51 -0500

    Create constantes.java

---
+++ algoritmos_usados/constantes.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class codigos {
+	public static void main(String arg[]) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		PrintWriter pw = new PrintWriter(System.out);
+		int a = 20;// variable modificable
+		final int x = 90;// constante fija sin opcion de modificar.
+		pw.close();
+		sc.close();
+	}
+}
