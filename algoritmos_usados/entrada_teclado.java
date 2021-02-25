67aa92e48dc2f69827077041f6fa4fb2cc72c593
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-02-24 21:51:57 -0500

    Create entrada_teclado.java

---
+++ algoritmos_usados/entrada_teclado.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class codigos {
+	public static void main(String arg[]) {
+		  String str;
+          System.out.print("como te llamas:  ");
+          str = System.console().readLine();
+          System.out.println("Hola " + str + 
+        ", ¡bienvenido al curso de java para principiantes!");
+	}
+}
