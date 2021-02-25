86bb3b03eba373f7d469522f7bfc0f1c0879a3dc
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-02-24 22:07:12 -0500

    Create length.java

---
+++ algoritmos_usados/length.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int vec []= new int [a];
+		System.out.println("El tamaño del vector es de: "+ vec.length+" espacios de memoria");
+	}
+}
