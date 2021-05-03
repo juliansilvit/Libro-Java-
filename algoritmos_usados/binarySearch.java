2496dec3dce40438a9d0d9ce52848dbd4ef54060
juliansilvit <62625597+juliansilvit@users.noreply.github.com>
2021-05-03 10:11:58 -0500

    Create binarySearch.java

---
+++ algoritmos_usados/binarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    
+   static int binarySearch(int arr[], int l, int r, int x){
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] == x) {
+                return mid;
+            }
+            if (arr[mid] > x) {
+                return binarySearch(arr, l, mid - 1, x);
+            }
+            return binarySearch(arr, mid + 1, r, x);
+        }
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {2, 3, 4, 10, 40};
+        int n = arr.length;
+        int x = 10;
+        int result = binarySearch(arr, 0, n - 1, x);
+        if (result == -1) {
+            System.out.println("Elemento no presente");
+        } else {
+            System.out.println("Element encontrado en la posición " + result);
+        }
+    }
+}
